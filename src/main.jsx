ad7745d6665b0503b3f54929e0e4a214c627ae4d
user <shanakaran23@gmail.com>
2024-12-02 23:36:44 +0530

    fixed an issue and set up tailwind

--- src/main.jsx
+++ src/main.jsx
@@ -1,7 +1,5 @@
+import ReactDOM from 'react-dom/client'; // Correct import
+import './index.css';
+import App from './App';
 
-import { createRoot } from 'react-dom/client'
-import './index.css'
-import App from './App.jsx'
-
-createRoot(document.getElementById('root')).render
-  
+ReactDOM.createRoot(document.getElementById('root')).render(<App />);
