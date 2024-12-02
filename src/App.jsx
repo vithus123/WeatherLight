4d4c841626137808e9219dd2622308909ec3b0de
user <shanakaran23@gmail.com>
2024-12-02 22:32:11 +0530

    initial commit

---
+++ src/App.jsx
@@ -0,0 +1,10 @@
+import { FaReact } from "react-icons/fa";
+const App = () => {
+  return (
+    <div>
+    <h1 className="text-7xl font-bold text-red-400 text-red">Helloaa</h1>
+</div>
+  );
+};
+
+export default App;
